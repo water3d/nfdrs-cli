50fa560e5e42b48de313aef212199b4fad4421cb
Nick Santos <nickrsan@users.noreply.github.com>
2024-06-11 16:57:47 -0700

    Create build.sh
    Create a build script that can be used instead of the dockerfile

---
+++ build.sh
@@ -0,0 +1,12 @@
+cd src
+tar xvf config4cpp.tar
+cd config4cpp
+sudo mkdir /usr/include/config4cpp
+sudo cp config4cpp/include/config4cpp/* /usr/include/config4cpp
+LD_LIBRARY_PATH=`pwd`/lib
+make clean
+make
+cd ..
+cp ../makefile_ns_updated ./Makefile
+make clean
+make
