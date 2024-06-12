48b6ccc6e9268cf994d48c59c61bdc63bed11cfa
Nick Santos <nickrsan@users.noreply.github.com>
2024-06-12 10:02:22 -0700

    Fix setting LD_LIBRARY_PATH to append new items, remove extraneous line in build script

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@ tar xvf config4cpp.tar
 cd config4cpp
 sudo mkdir /usr/include/config4cpp
 sudo cp config4cpp/include/config4cpp/* /usr/include/config4cpp
-LD_LIBRARY_PATH=`pwd`/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib
 make clean
 make
 cd ..
-cp ../makefile_ns_updated ./Makefile
 make clean
 make
 mv ./NFDRScliV4 ../NFDRScliV4
